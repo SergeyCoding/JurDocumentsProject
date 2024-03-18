aef2d6e40f447c59a3ed216fe0ef954894b95169
SergeyXP <s.pusthotin@gmail.com>
2024-03-18 21:13:39 +0400

    1

--- JurDocs/ViewModel/MainViewModel.cs
+++ JurDocs/ViewModel/MainViewModel.cs
@@ -1,15 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using JurDocs;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Windows.Input;
 
 namespace JurDocs.ViewModel
 {
-    internal class MainViewModel
+    internal class MainViewModel:INotifyPropertyChanged
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        //public ICommand BtnDogovors { get; set; }
 
-        public ICommand BtnDogovors { get; set; }
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private RelayCommand aaa1;
+        public ICommand ClickSpravki => aaa1 ??= new RelayCommand(Performaaa);
+
+        private void Performaaa(object commandParameter)
+        {
+        }
+
+        private RelayCommand btnDogovors;
+        public ICommand BtnDogovors => btnDogovors ??= new RelayCommand((a) => { });
+
+        private void PerformBtnDogovors(object commandParameter)
+        {
+        }
+
+        protected bool SetProperty<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if (!Equals(field, newValue))
+            {
+                field = newValue;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+                return true;
+            }
+
+            return false;
+        }
+
+        private object docList1;
+
+        public object docList { get => docList1; set => SetProperty(ref docList1, value); }
     }
 }
