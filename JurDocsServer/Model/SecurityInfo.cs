3daa340a786e43e123ebb38edac5e12ad9324f78
SergeyCoding <31168007+SergeyCoding@users.noreply.github.com>
2024-03-20 08:05:36 +0400

    T1 (#2)
    * Добавлен data.json
    
    * Добавлен data.json
    
    * 1

---
+++ JurDocsServer/Model/SecurityInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace JurDocsServer.Model
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class SecurityInfo
+    {
+        public List<Catalog>? Catalogs { get; set; }
+        public List<User>? Users { get; set; }
+    }
+}
---
+++ JurDocsServer/Model/User.cs
@@ -0,0 +1,21 @@
+﻿namespace JurDocsServer.Model
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Name { get; set; } = string.Empty;
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Path { get; set; } = string.Empty;
+    }
+}
