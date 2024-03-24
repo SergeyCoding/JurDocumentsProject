31d7680c8788a73d35f02a59e59e10a57a3e3fe7
SergeyXP <s.pusthotin@gmail.com>
2024-03-24 13:11:06 +0400

    # Current Tasks - добавить БД sqlite

---
+++ DbModel/User.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace DbModel
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Login { get; set; }
+        public string? Password { get; set; }
+        public string? Path { get; set; }
+
+        class Configuration : IEntityTypeConfiguration<Token>
+        {
+            public void Configure(EntityTypeBuilder<Token> builder)
+            {
+            }
+        }
+    }
+}
